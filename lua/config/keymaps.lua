be1961fbe17d6015ad1a0ac666fd6bf40efb8584
Metin Yalcinkaya <metinyalcinkaya@hotmail.com.au>
2026-02-24 07:09:31 +1100

    rewrote keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,67 +1,73 @@
--- clear highlights
-vim.keymap.set("n", "<Esc>", "<cmd>nohl<CR>")
+local function map(keys, func, opts, mode)
+    mode = mode or "n"
+    opts = type(opts) == "string" and { desc = opts } or opts
+    vim.keymap.set(mode, keys, func, opts)
+end
 
--- Diagnostic keymaps
-vim.keymap.set("n", "<Leader>q", vim.diagnostic.setqflist, { desc = "Quickfix list" })
-
--- Rebinds arrowkeys to use hjkl while using Glove80
-vim.keymap.set("n", "<Left>", "h", { noremap = true, silent = true })
-vim.keymap.set("n", "<Down>", "j", { noremap = true, silent = true })
-vim.keymap.set("n", "<Up>", "k", { noremap = true, silent = true })
-vim.keymap.set("n", "<Right>", "l", { noremap = true, silent = true })
-
--- Keybinds to make split navigation easier.
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-
-vim.keymap.set("n", "<C-Left>", "<C-h>", { noremap = false, silent = true })
-vim.keymap.set("n", "<C-Down>", "<C-j>", { noremap = false, silent = true })
-vim.keymap.set("n", "<C-Up>", "<C-k>", { noremap = false, silent = true })
-vim.keymap.set("n", "<C-Right>", "<C-l>", { noremap = false, silent = true })
-
--- Remove
-for _, bind in ipairs({ "gri", "grr" }) do
+-- Remove default bindings (for Snacks rebinding)
+for _, bind in ipairs({ "gri", "grr", "grt", "gO" }) do
     pcall(vim.keymap.del, "n", bind)
 end
 
+-- Clear highlights
+map("<Esc>", "<Cmd>nohl<CR>")
+
+-- Diagnostic keymaps
+map("<Leader>q", vim.diagnostic.setqflist, "Quickfix List")
+
+-- Rebinds arrowkeys to use hjkl while using Glove80
+map("<Left>", "h", { noremap = true, silent = true })
+map("<Down>", "j", { noremap = true, silent = true })
+map("<Up>", "k", { noremap = true, silent = true })
+map("<Right>", "l", { noremap = true, silent = true })
+
+-- Split navigation
+map("<C-h>", "<C-w><C-h>", "Move focus to the left window")
+map("<C-j>", "<C-w><C-j>", "Move focus to the lower window")
+map("<C-k>", "<C-w><C-k>", "Move focus to the upper window")
+map("<C-l>", "<C-w><C-l>", "Move focus to the right window")
+
+map("n", "<C-Left>", "<C-h>", { noremap = false, silent = true })
+map("n", "<C-Down>", "<C-j>", { noremap = false, silent = true })
+map("n", "<C-Up>", "<C-k>", { noremap = false, silent = true })
+map("n", "<C-Right>", "<C-l>", { noremap = false, silent = true })
+
 -- Snacks
-vim.keymap.set("n", "<Leader>n", function()
+map("<Leader>n", function()
     Snacks.picker.notifications()
-end, { desc = "Notification History" })
-vim.keymap.set("n", "<Leader>fc", function()
+end, "Notification History")
+map("<Leader>fc", function()
     Snacks.picker.files({ cwd = vim.fn.stdpath("config") })
-end, { desc = "Config" })
-vim.keymap.set("n", "<Leader>ff", function()
+end, "Config")
+map("<Leader>ff", function()
     Snacks.picker.files({ hidden = true })
-end, { desc = "Files" })
-vim.keymap.set("n", "<Leader>fi", function()
+end, "Files")
+map("<Leader>fi", function()
     Snacks.picker.icons()
-end, { desc = "Icons" })
-vim.keymap.set("n", "<Leader>fh", function()
+end, "Icons")
+map("<Leader>fh", function()
     Snacks.picker.help()
-end, { desc = "Help" })
-vim.keymap.set("n", "<Leader>fd", function()
+end, "Help")
+map("<Leader>fd", function()
     Snacks.picker.diagnostics_buffer()
-end, { desc = "Diagnostics (Buffer)" })
-vim.keymap.set("n", "<Leader>fD", function()
+end, "Diagnostics (Buffer)")
+map("<Leader>fD", function()
     Snacks.picker.diagnostics()
-end, { desc = "Diagnostics" })
-vim.keymap.set("n", "<Leader>fg", function()
+end, "Diagnostics")
+map("<Leader>fg", function()
     Snacks.picker.grep()
-end, { desc = "Diagnostics" })
-vim.keymap.set("n", "<Leader>fu", function()
-    Snacks.picker.undo()
-end, { desc = "Undotree" })
-vim.keymap.set("n", "<Leader>fb", function()
+end, "Grep")
+map("<Leader>fb", function()
     Snacks.picker.lines()
-end, { desc = "Grep Buffer" })
-vim.keymap.set("n", "<Leader>ft", function()
+end, "Grep Current Buffer")
+map("<Leader>fu", function()
+    Snacks.picker.undo()
+end, "Undotree")
+map("<Leader>ft", function()
     Snacks.picker.todo_comments()
-end, { desc = "Todo" })
+end, "Todo")
 
 -- Conform
-vim.keymap.set("", "<Leader>fm", function()
+map("<Leader>fm", function()
     require("conform").format({ async = true, lsp_format = "fallback" })
-end, { desc = "Format buffer" })
+end, "Format Buffer", "")
