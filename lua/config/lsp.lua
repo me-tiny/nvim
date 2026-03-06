5d84decaf4b9a870ff2bab9b1265cca9a22299ed
Metin Yalcinkaya <metinyalcinkaya@hotmail.com.au>
2026-03-06 18:43:43 +1100

    removed blink.cmp capabilities from lsp config; blink does itself

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -105,7 +105,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
     once = true,
     callback = function()
-        vim.lsp.config("*", { capabilities = require("blink.cmp").get_lsp_capabilities(nil, true) })
+        vim.lsp.config("*", { capabilities = { textDocument = { semanticTokens = { multilineTokenSupport = true } } } })
         local servers = vim.iter(vim.api.nvim_get_runtime_file("lsp/*.lua", true))
             :map(function(file)
                 return vim.fn.fnamemodify(file, ":t:r")
