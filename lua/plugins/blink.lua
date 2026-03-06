93ba51b1ed14a29e0d1453d4d70d4075a42fceff
Metin Yalcinkaya <metinyalcinkaya@hotmail.com.au>
2026-03-06 18:40:15 +1100

    blink.cmp event InsertEnter -> VeryLazy

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,7 +1,7 @@
 return {
     {
         "saghen/blink.cmp",
-        event = "InsertEnter",
+        event = "VeryLazy",
         build = "cargo +nightly build --release",
         opts = {
             snippets = {
