cb18ef7186a614d99437d22440cb2c7354a12699
Metin Yalcinkaya <metinyalcinkaya@hotmail.com.au>
2026-03-30 22:27:36 +1100

    changed gitsigns symbols

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -21,19 +21,18 @@ return {
         end,
         opts = {
             signs = {
-                add = { text = "▎" },
-                change = { text = "▎" },
-                delete = { text = "" },
-                topdelete = { text = "" },
-                changedelete = { text = "▎" },
-                untracked = { text = "▎" },
+                add = { text = "+" },
+                change = { text = "~" },
+                delete = { text = "_" },
+                topdelete = { text = "‾" },
+                changedelete = { text = "~" },
             },
             signs_staged = {
-                add = { text = "▎" },
-                change = { text = "▎" },
-                delete = { text = "" },
-                topdelete = { text = "" },
-                changedelete = { text = "▎" },
+                add = { text = "+" },
+                change = { text = "~" },
+                delete = { text = "_" },
+                topdelete = { text = "‾" },
+                changedelete = { text = "~" },
             },
             on_attach = function(bufnr)
                 local gitsigns = require("gitsigns")
