ef91db6bf2e493e5a2ce15b443e9596a311a8b09
Metin Yalcinkaya <metinyalcinkaya@hotmail.com.au>
2026-04-21 23:09:41 +1000

    gitsigns error fix

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -6,7 +6,7 @@ return {
             vim.api.nvim_create_autocmd("BufRead", {
                 group = vim.api.nvim_create_augroup("GitSignsLazyLoad", { clear = true }),
                 callback = function()
-                    vim.fn.jobstart({ "git", "-C", vim.fn.expand("%:h"), "rev-parse" }, {
+                    vim.fn.jobstart({ "git", "-C", vim.loop.cwd(), "rev-parse" }, {
                         on_exit = function(_, return_code)
                             if return_code == 0 then
                                 vim.api.nvim_del_augroup_by_name("GitSignsLazyLoad")
