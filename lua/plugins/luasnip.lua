ca9bf72350891700e667d82b74813cb773a46897
Metin Yalcinkaya <metinyalcinkaya@hotmail.com.au>
2026-07-04 22:06:59 +1000

    feat: deleting lazy.nvim files, fully commiting to vim.pack

--- lua/plugins/luasnip.lua
+++
@@ -1,9 +0,0 @@
-return {
-    {
-        "L3MON4D3/LuaSnip",
-        event = "VeryLazy",
-        build = "make install_jsregexp",
-        dependencies = "rafamadriz/friendly-snippets",
-        opts = {},
-    },
-}
--- lua/plugins/mini-ai.lua
+++
@@ -1,9 +0,0 @@
-return {
-    {
-        "nvim-mini/mini.ai",
-        event = "BufReadPre",
-        opts = {
-            n_lines = 500,
-        },
-    },
-}
